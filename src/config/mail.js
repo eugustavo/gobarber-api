efb4ceb10a07be1d77c4ab2401f4d95e6270222e
Gustavo Souza <gustavo.ifto@gmail.com>
2020-02-21 18:30:30 -0300

    Setting up email sending

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '44da582452fdb5',
+    pass: 'f11f0b3e6a5d8a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com',
+  },
+};
